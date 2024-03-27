8a5d491ec98533300ea556495d9f03d87954be57
Sangreal2021 <hiw051515@gmail.com>
2024-03-27 15:38:04 +0900

    20240327_comp

--- batch-campus/src/main/java/com/fastcampus/batchcampus/DormantBatchJob.java
+++ batch-campus/src/main/java/com/fastcampus/batchcampus/DormantBatchJob.java
@@ -36,13 +36,18 @@ public class DormantBatchJob {
             while (true) {
 
                 // 1. 유저를 조회
+                // PageRequest 객체를 생성하여 페이지 번호(pageNo), 페이지 당 항목 수(여기서는 1),
+                // 그리고 ID를 기준으로 오름차순 정렬하는 방식을 지정함. 이는 한 번에 하나의 고객 데이터만 처리하도록 함.
                 final PageRequest pageRequest = PageRequest.of(pageNo, 1, Sort.by("id").ascending());
                 final Page<Customer> page = customerRepository.findAll(pageRequest);
 
                 final Customer customer;
+                // 조회된 페이지가 비어 있으면 더 이상 처리할 고객이 없으므로 루프를 종료
                 if (page.isEmpty()) {
                     break;
                 } else {
+                    // 페이지가 비어 있지 않다면 페이지 번호를 증가시키고,
+                    // 페이지에서 첫 번째 고객 데이터를 가져와 customer 변수에 할당
                     pageNo++;
                     customer = page.getContent().get(0);
                 }
